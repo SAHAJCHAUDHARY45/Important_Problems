dd452e9da4e96a0229afec176e3a924e5d5885a6
Sahaj Chaudhary <patelsahaj517@gmail.com>
2025-01-29 22:53:54 +0530

    Array Problems

---
+++ Array/A02_ReverseArray.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.Arrays;
+
+public class A02_ReverseArray {
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start <= end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        for (int ele: arr){
+            System.out.print(ele+ " ");
+        }
+    }
+
+}
